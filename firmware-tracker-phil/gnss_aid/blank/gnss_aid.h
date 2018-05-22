d984e927af667195a2ec67af6bd148a1327334ec
Phil Crump <phil@philcrump.co.uk>
2018-05-22 02:23:11 +0100

    Add blank gnss_aid template to git.

---
+++ firmware-tracker-phil/gnss_aid/blank/gnss_aid.h
@@ -0,0 +1,13 @@
+#ifndef __GNSS_AID_H__
+#define __GNSS_AID_H__
+
+#define GNSS_SEND_AID_ALMANAC() \
+_delay_ms(5);
+
+#define GNSS_SEND_AID_AUXILIARY() \
+_delay_ms(5);
+
+#define GNSS_SEND_AID_UBXOFFLINE() \
+_delay_ms(5);
+
+#endif /* __GNSS_AID_H__ */
