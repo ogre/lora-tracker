cbfc49af1e7b342139734aec3dcdb0b8f4499759
Phil Crump <phil@philcrump.co.uk>
2018-07-01 23:57:32 +0100

    firmware-tracker: Remove redundant data printout.

--- firmware-tracker-phil/configs/test.c
+++ firmware-tracker-phil/configs/test.c
@@ -13,6 +13,24 @@
 
 int main(void)
 {
+    #ifdef GNSS_AID_AUXILIARY
+        uint64_t auxiliary_age = time(NULL) - GNSS_AID_TIMESTAMP;
+        printf("GNSS Ionospheric Aid Enabled (");
+        if(auxiliary_age > 3600)
+        {
+            printf("%.1f hours", (double)auxiliary_age / 3600.0);
+        }
+        else if(auxiliary_age > 60)
+        {
+            printf("%.1f minutes", (double)auxiliary_age / 60.0);
+        }
+        else
+        {
+            printf("<1 minute");
+        }
+        printf(" old)\n");
+    #endif
+
     #ifdef GNSS_AID_ALMANAC
         uint64_t almanac_age = time(NULL) - GNSS_AID_TIMESTAMP;
         printf("GNSS Almanac Aid Enabled (");
